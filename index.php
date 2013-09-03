e05fdefdcb24913fa7532aa1294336c7007f21f4
Steve <evenstevens280@gmail.com>
2013-09-03 19:39:48 +0100

    deleting php

--- index.php
+++
@@ -1,5 +0,0 @@
-<title><?=$_SERVER['HTTP_HOST']?> is currently under construction</title>
-<font face="tahoma, arial, helvetica, sans-serif">
-<p>Welcome to <strong><?=$_SERVER['HTTP_HOST']?></strong>, a site recently created using our clustered <?=PHP_OS?> web hosting.</p>
-<p>If you're seeing this page instead of your website, please replace or remove the default index.php from your public_html directory.</p>
-</font>
